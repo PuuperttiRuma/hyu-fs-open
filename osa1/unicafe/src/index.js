f4145547a3530a92c8893d73dc28b08da789e320
PuuperttiRuma <thaapane@student.jyu.fi>
2020-08-13 14:16:07 +0300

    1.6 Feedback counters works

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good+1)}>Good</button>
+      <button onClick={() => setNeutral(neutral+1)}>Neutral</button>
+      <button onClick={() => setBad(bad+1)}>Bad</button>
+
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +29,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

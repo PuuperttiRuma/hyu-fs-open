afe0336f80ab370ed36fb8966d5bd10b5c06e4be
Tommi Haapanen <thaapane@student.jyu.fi>
2021-04-28 08:08:52 +0300

    models.js eriytetty

--- osa4/bloglist/app.js
+++ osa4/bloglist/app.js
@@ -2,17 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const Blog = require('./models/blog.js')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,

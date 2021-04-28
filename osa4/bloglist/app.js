ded09a4d54f74fc04399e3d2b5ed819fddda8f6a
Tommi Haapanen <thaapane@student.jyu.fi>
2021-04-28 08:21:31 +0300

    4.2 Ohjelma eriytetty erillisiin osiin

--- osa4/bloglist/app.js
+++ osa4/bloglist/app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const Blog = require('./models/blog.js')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 
 const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
@@ -15,19 +18,6 @@ mongoose.connect(config.MONGODB_URI, {
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app

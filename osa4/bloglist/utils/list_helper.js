30b98e1d3bdf5a6729703a408ad63b4b5c31a82b
Tommi Haapanen <thaapane@student.jyu.fi>
2021-04-29 09:34:48 +0300

    4.4 totalLikes ja testit

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -3,4 +3,15 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = {dummy}
+/**
+test('of empty list is zero')
+test('when list has only one blog equals the likes of that')
+test('of a bigger list are calculated correctly')
+ */
+const totalLikes = (blogs) => {  
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce( (total, blog) => total + blog.likes, 0)
+}
+
+module.exports = {dummy, totalLikes}
